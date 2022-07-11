aa1e1c889689744ed7c0250154e769864dfaec58
onskulit <dislaty@mail.com>
2022-07-11 15:17:38 +0300

    feat: add state

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,5 @@
 import './style.scss';
 import create from './scripts/utils/create';
-
-interface Good {
-  id: number;
-  name: string;
-  year: number;
-}
-
-const goods: Good[] = [
-  {
-    id: 1,
-    name: 'Jordan 1',
-    year: 2015
-  },
-  {
-    id: 2,
-    name: 'Jordan 2',
-    year: 2012
-  }
-]
+import goods from './scripts/state';
 
 goods.forEach(good => create('div', 'good', `${good.name} / ${good.year}`, (document.querySelector('.goods') as HTMLElement), ['id', good.id.toString()]))
---
+++ src/scripts/state.ts
@@ -0,0 +1,25 @@
+interface Good {
+  id: number;
+  name: string;
+  year: number;
+}
+
+const goods: Good[] = [
+  {
+    id: 1,
+    name: 'Jordan 1',
+    year: 2015
+  },
+  {
+    id: 2,
+    name: 'Jordan 2',
+    year: 2012
+  },
+  {
+    id: 3,
+    name: 'Jordan 3',
+    year: 2011
+  }
+]
+
+export default goods;
