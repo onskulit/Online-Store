8378229d4f28f4353a55acc6e1393a2a24e5e2c6
onskulit <dislaty@mail.com>
2022-07-11 15:06:29 +0300

    feat: add create function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
 import './style.scss';
+import create from './scripts/utils/create';
+
+interface Good {
+  id: number;
+  name: string;
+  year: number;
+}
+
+const goods: Good[] = [
+  {
+    id: 1,
+    name: 'Jordan 1',
+    year: 2015
+  },
+  {
+    id: 2,
+    name: 'Jordan 2',
+    year: 2012
+  }
+]
+
+goods.forEach(good => create('div', 'good', `${good.name} / ${good.year}`, (document.querySelector('.goods') as HTMLElement), ['id', good.id.toString()]))
