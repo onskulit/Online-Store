47ea828e346bb5c1421163c15d554815952fcc75
onskulit <dislaty@mail.com>
2022-07-11 13:03:57 +0300

    feat: add scss

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
-console.log('hello');
-console.log('by');
-console.log('by');
+import './style.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,15 @@ const baseConfig = {
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+              test: /\.s[ac]ss$/i,
+              use: [
+                // Creates `style` nodes from JS strings
+                "style-loader",
+                // Translates CSS into CommonJS
+                "css-loader",
+                // Compiles Sass to CSS
+                "sass-loader",
+              ]
             },
             { 
                 test: /\.ts$/i, 
