a251c57346b71b1a23b26b0a1b7459dfd645becf
onskulit <dislaty@mail.com>
2022-07-13 20:42:10 +0300

    feat: update items cards. Add info and update layout

--- src/scripts/components/Goods/Goods.ts
+++ src/scripts/components/Goods/Goods.ts
@@ -19,10 +19,15 @@ class Goods implements IGoodsContainer {
       (document.querySelector('.goods') as HTMLElement).innerHTML = 'No such items found';
     }
     goods.forEach(good => {
-      const item: HTMLElement = create('div', 'good', `${good.name} / ${good.year}`, 
+      const item: HTMLElement = create('div', 'good', `${good.name}`, 
       (document.querySelector('.goods') as HTMLElement), ['id', good.id.toString()]);
       const img: HTMLImageElement = create('img', 'good-img', null, item) as HTMLImageElement;
       img.src = good.imgLink;
+      const info: HTMLElement = create('ul', 'good-info', null, item) as HTMLImageElement;
+      Object.entries(good).forEach(([key, value]) => {
+        if (key !== 'id' && key !== 'name' && key !== 'imgLink')
+        create('li', 'good-info__item', `${key}: ${value}`, item) as HTMLElement;
+      })
     })
   }
 }
